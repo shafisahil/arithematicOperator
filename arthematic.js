246848f72515790cdf8cd821292de9795c882bbc
shafisahil <shafisahil7868@gamil.com>
2021-04-29 03:08:08 -0700

    arithematicOperator

---
+++ arthematic.js
@@ -0,0 +1,84 @@
+let num = 6;
+let num2 = 8;
+let num3 = 18;
+let num4 = 20;
+
+///////////////for ada//
+console.log(num + num2);
+let a = (num + num2);
+console.log(typeof(a));
+
+console.log(num4 + num3);
+let b = (num4 + num3);
+console.log(typeof(b));
+
+
+//// for subtration////////
+
+console.log(num4 - num);
+let c = (num4 - num);
+console.log(typeof(c));
+console.log(num3 - num2);
+let d = (num3 - num2);
+console.log(typeof(d));
+
+//////for multiplication/////
+console.log(num * num4);
+let e = (num * num4);
+console.log(typeof(e));
+console.log(num2 * num3);
+let f = (num2 * num3);
+console.log(typeof(f));
+
+///////for division///////
+console.log(num4 / num);
+let g = (num4  / num);
+console.log(typeof(g));
+console.log(num2 / num);
+let h = (num2 / num);
+console.log(typeof(h));
+/////////////////reaminder////
+console.log(num4 % num);
+let i = (num4 % num);
+console.log(typeof(i));
+console.log(num3 % num2);
+let j = (num3 % num2);
+console.log(typeof(j));
+//////////////// for square//////
+let square = num**2;
+console.log(square)
+let square1 =num3**6;
+console.log(square1);
+
+////////////////////incriment//////////
+let incriment = num3++;
+console.log(incriment);
+let incriment1 = num4++;
+console.log(incriment1);
+////////////////////////decriment////
+let decriment = num--;
+console.log(decriment);
+let decriment1 = num2--;
+console.log(decriment1);
+/////////////argument substration/
+num -= 40;
+console.log(num);
+num2 -= 10;
+console.log(num2);
+/////argument add//////
+num3 += 20;
+console.log(num3);
+num4 += 60;
+console.log(num4);
+////////////argument for mul//////
+num2 *= 8;
+console.log(num2);
+num3 *= 12;
+console.log(num3);
+////////////////argument fordivision//////
+num4 /= 14;
+console.log(num4);
+num3 /= 2;
+console.log(num3)
+
+
